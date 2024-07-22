b72add929c03633a1e45c5dd8ec4c0edb0a6786a
“xiaowang” <1844912514@qq.com>
2024-07-22 11:47:08 +0800

    end

--- src/main/java/com/yksj/monitor/controller/ThymeleafController.java
+++ src/main/java/com/yksj/monitor/controller/ThymeleafController.java
@@ -1,23 +1,31 @@
 package com.yksj.monitor.controller;
 
-import com.yksj.monitor.entity.Stu;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.*;
 
 @Controller
 @Slf4j
 @RequestMapping("thyme")
 public class ThymeleafController {
 
+    @Autowired
+    private ServletContext servletContext;
+
     @GetMapping("view")
-    public String view(Model model) {
+    public String view(Model model, HttpServletRequest request) {
         // 1.分开传递
         // model.addAttribute("name", "lxw");
         // model.addAttribute("age", 11);
@@ -29,14 +37,84 @@ public class ThymeleafController {
 //        model.addAttribute(stu);
 
 //        3.传递map
-        Map<String,String> map = new HashMap<>();
-        map.put("name","test22");
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "test22");
         model.addAllAttributes(map);
 
 
-        Date birthday= new Date();
-        model.addAttribute("birthday",birthday);
+        Date birthday = new Date();
+        model.addAttribute("birthday", birthday);
+
+        Integer sex = 1;
+        model.addAttribute("sex", sex);
+
+        List<String> myList = new ArrayList();
+        myList.add("java");
+        myList.add("html");
+        myList.add("php");
+        myList.add("go");
+
+        Map<String, Object> myMap = new HashMap<>();
+        myMap.put("name", "lxw");
+        myMap.put("sex", 1);
+        myMap.put("list", myList);
+
+        model.addAttribute("myMap", myMap);
+        model.addAttribute("myList", myList);
+
+        request.setAttribute("englishName", "xd");
+        request.getSession().setAttribute("token", "token-121212");
 
         return "teacher"; //模版文件名
+    }
+
+
+    @Autowired
+    TemplateEngine templateEngine;
+
+    @GetMapping("createHtml")
+    @ResponseBody
+    public String createHtml(Model model, HttpServletRequest request) throws Throwable {
+//        3.传递map
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "test22");
+        model.addAllAttributes(map);
+
+
+        Date birthday = new Date();
+        model.addAttribute("birthday", birthday);
+
+        Integer sex = 1;
+        model.addAttribute("sex", sex);
+
+        List<String> myList = new ArrayList();
+        myList.add("java");
+        myList.add("html");
+        myList.add("php");
+        myList.add("go");
+
+        Map<String, Object> myMap = new HashMap<>();
+        myMap.put("name", "lxw");
+        myMap.put("sex", 1);
+        myMap.put("list", myList);
+
+        model.addAttribute("myMap", myMap);
+        model.addAttribute("myList", myList);
+
+        //生成静态页面 开始静态化
+        Context context = new Context();
+        context.setVariable("myMap", myMap);
+        context.setVariable("myList", myList);
+        context.setVariable("birthday", birthday);
+        context.setVariable("sex", sex);
+//        context.setVariable("map", map);
+        context.setVariable("name", map);
+
+        //定义保存的目录
+        String path = "./";
+        Writer out = new FileWriter(path + "/teacher.html");
+        templateEngine.process("teacher", context, out);
+        out.close();
+        return "ok"; //模版文件名
     }
 }
--- src/test/java/com/yksj/monitor/entity/StudentTest.java
+++ src/test/java/com/yksj/monitor/entity/StudentTest.java
@@ -1,7 +1,25 @@
 package com.yksj.monitor.entity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.yksj.monitor.service.StuService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.util.UUID;
 
+@SpringBootTest //表示当前会被sprint boot 加载为测试类
 class StudentTest {
+    @Autowired
+    StuService stuService;
+
+    @Test
+    public void testStu() {
+
+        Stu stu = new Stu();
+        stu.setName(UUID.randomUUID().toString());
+        stu.setAge(22);
+        stu.setEmail("22@qq.com");
+        stuService.savaStu(stu);
+    }
+
 
 }
